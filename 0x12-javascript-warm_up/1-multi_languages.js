fcefd94b63c397ea317ab01d32c44707dae5f5a0
ziontsion <tsionnew@gmail.com>
2023-06-12 05:38:33 -0700

    javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//script that prints 3 lines
+
+const var1 = 'C is fun';
+const var2 = 'Python is cool';
+const var3 = 'JavaScript is amazing';
+console.log (myVar1);
+console.log (myVar2);
+console.log (myVar3);
